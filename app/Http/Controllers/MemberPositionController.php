2094869efc6c76c56adede99d682ecc2f2d76166
Freddy T <freddy@freddychan.dev>
2025-03-21 00:31:49 -0400

    install pint, larastan and run linter

--- app/Http/Controllers/MemberPositionController.php
+++ app/Http/Controllers/MemberPositionController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class MemberPositionController extends Controller
 {
     //
--- database/seeders/EventScheduleItemsSeeder.php
+++ database/seeders/EventScheduleItemsSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class EventScheduleItemsSeeder extends Seeder
