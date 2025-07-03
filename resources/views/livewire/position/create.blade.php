8f2eeb64e8b4900a4c676cfc2f1d3cb9bd8c2476
Freddy T <hi@freddykresna.com>
2025-07-03 00:51:37 -0400

    clean up unnecessary files

--- resources/views/livewire/position/create.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <form class="my-6 w-full space-y-6">
-        <flux:input wire:model="name" label="Position Name" type="text" autofocus required/>
-        <flux:input wire:model="description" label="Description" type="text" />
-        <flux:button button>Cancel</flux:button>
-        <flux:button variant="primary">Save</flux:button>
-    </form>
-</div>
